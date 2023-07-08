c4999c4487e8b850d4947283fa077c285a6a3f79
Clara Local <C.Lilje@sms.ed.ac.uk>
2023-07-08 11:24:28 +0200

    first basic functions and all the inputs

--- team2-ing/calculator_backend.py
+++ team2-ing/calculator_backend.py
@@ -1,7 +1,9 @@
 import sys
 import pandas as pd
 import numpy as np
-
+from datetime import datetime
+#living_wage = 
+minimum_wage = 12
 class User:
     def __init__(self, params):
         self.timestep = params[0]
@@ -23,17 +25,18 @@ class User:
         self.caring_hours_other = params[16]
         self.external_caring_hours_other = params[17]
         self.spending_external_care_other = params[18]
-        self.household_hours = params[19]
-        self.groceries = params[20]
-        self.travel = params[21]
-        self.transportation = params[22]
-        self.bills = params[23]
-        self.leisure = params[24]
-        self.mental_load = params[25]
-        self.preferences_work = params[26]
-        self.preferences_pension = params[27]
-        self.preferences_care = params[28]
-        
+        self.other_benefits = params[19]
+        self.household_hours = params[20]
+        self.groceries = params[21]
+        self.travel = params[22]
+        self.transportation = params[23]
+        self.bills = params[24]
+        self.leisure = params[25]
+        self.mental_load = params[26]
+        self.preferences_work = params[27]
+        self.preferences_pension = params[28]
+        self.preferences_care = params[29]
+        self.name = params[30]
         
     def pension(self):
         self.pension = self.earning_points*self.access_factor*self.pension_value*self.pension_factor
@@ -43,23 +46,72 @@ class User:
 
     def comp_pension(self):
         min_pension = 11000
-
-
-    def my_function(self):
-        print("Param1:", self.param1)
-        print("Param2:", self.param2)
-    
-    def compare_params(self, other_instance):
-        self_sum = self.param1 + self.param2
-        other_sum = other_instance.param1 + other_instance.param2
-        
-        if self_sum > other_sum:
-            print("This instance has a larger sum of parameters.")
-        elif self_sum < other_sum:
-            print("The other instance has a larger sum of parameters.")
+        if self.pension < min_pension:
+            self.pension_enough = False
         else:
-            print("Both instances have the same sum of parameters.")
+            self.pension_enough = True
 
+
+    def care(self):
+        self.child_care_wage = self.caring_hours_child*4 *minimum_wage - self.child_benefits
+        if self.external_caring_hours_child != 0:
+            self.adapted_child_care_wage = self.spending_external_care_child/self.external_caring_hours_child *self.caring_hours_child*4
+        else:
+            self.adapted_child_care_wage = 0
+        self.other_care_wage = self.caring_hours_other*4 *minimum_wage - self.other_benefits
+        if self.external_caring_hours_other != 0:
+            self.adapted_other_care_wage = self.spending_external_care_other/self.external_caring_hours_other *self.caring_hours_other*4
+        else:
+            self.adapted_other_care_wage = 0
+        
+        self.total_care_wage = self.child_care_wage+self.other_care_wage
+
+    def tax_class_rec(self,other_instance):
+        if self.rel_status == 'married':
+            if self.tax_class == 4 and other_instance.tax_class ==4:
+                self.tax_class_rec = 'op1'
+            if self.tax_class == 5 or self.tax_class ==3:
+                if self.income == 0 or other_instance.income == 0:
+                    self.tax_class_rec == 'op3'
+                else:
+                    self.tax_class_rec == 'op4'
+        else:
+            self.tax_class_rec ='op2'
+    
+    def mental_load_burden(self,other_instance):
+        if self.mental_load > other_instance.mental_load:
+            self.mental_burden = True
+        elif self.mental_load <= other_instance.mental_load:
+            self.mental_burden = False
+        if self.mental_load == other_instance.mental_load:
+            self.shared_burden = True
+        else:
+            self.shared_burden = False
+    
+
+    def income_stats(self):
+        
+        date2 = datetime.strptime(self.timestep, '%Y-%m-%d')
+        date1 = datetime.strptime(self.parental_leave_start, '%Y-%m-%d')
+        time_passed = (date2.year - date1.year) * 12 + (date2.month - date1.month)
+
+        if self.parental_leave_duration == 0:
+            self.income_overview = self.salary
+            self.income_overview_plus_care = self.salary + self.total_care_wage
+            self.income_overview_plus_care_household = self.salary + self.total_care_wage +self.household_hours*minimum_wage
+        else:
+            if self.parental_leave_duration =< 12:
+                self.parental_wage = 0.65*self.salary*self.parental_leave_duration
+            else:
+                self.parental_wage = 0.65*self.salary*self.parental_leave_duration
+
+            time_no_parental_leave = time_passed-self.parental_leave_duration
+            self.income_overview = (self.salary*time_no_parental_leave + self.parental_wage)/time_passed
+            self.income_overview_plus_care = (self.salary*time_no_parental_leave+self.parental_wage)/time_passed + self.total_care_wage
+            self.income_overview_plus_care_household = (self.salary*time_no_parental_leave+self.parental_wage)/time_passed + self.total_care_wage +self.household_hours*minimum_wage
+    
+
+                
 user_ID = sys.argv[1]
 user_ID = 1
 
@@ -73,68 +125,15 @@ info_p2 = User_input[1].to_numpy()
 # Create an instance of MyClass with the input values
 partner1 = User(info_p1)
 partner2 = User(info_p2)
-
-# Call the function on the instances
-partner1.my_function()
-partner2.my_function()
-
-
-
-
-#gotta think about this section::
-class TreeNode:
-    def __init__(self, decision_condition=None, true_branch=None, false_branch=None, leaf_value=None, probability=1.0):
-        self.decision_condition = decision_condition
-        self.true_branch = true_branch
-        self.false_branch = false_branch
-        self.leaf_value = leaf_value
-        self.probability = probability
-
-class DecisionTree:
-    def __init__(self):
-        self.root = None
-    
-    def fit(self):
-        # Create the decision tree structure
-        # Manually specify the decision conditions, leaf values, and probabilities
-        
-        # Root node
-        self.root = TreeNode(decision_condition=('feature1', 'value1'), probability=0.8)
-        
-        # Level 1 nodes
-        node2 = TreeNode(decision_condition=('feature2', 'value2'), probability=0.6)
-        node3 = TreeNode(decision_condition=('feature3', 'value3'), probability=0.4)
-        
-        # Level 2 nodes
-        leaf1 = TreeNode(leaf_value='Outcome 1', probability=0.3)
-        leaf2 = TreeNode(leaf_value='Outcome 2', probability=0.2)
-        leaf3 = TreeNode(leaf_value='Outcome 3', probability=0.15)
-        leaf4 = TreeNode(leaf_value='Outcome 4', probability=0.1)
-        
-        # Assign the nodes as per the desired tree structure
-        self.root.true_branch = node2
-        self.root.false_branch = node3
-        node2.true_branch = leaf1
-        node2.false_branch = leaf2
-        node3.true_branch = leaf3
-        node3.false_branch = leaf4
-    
-    def predict(self, X):
-        predictions = []
-        for sample in X:
-            ranked_outcomes = self._traverse_tree(sample, self.root)
-            predictions.append(ranked_outcomes)
-        return predictions
-    
-    def _traverse_tree(self, sample, node):
-        if node.leaf_value is not None:
-            return [(node.leaf_value, node.probability)]
-        
-        feature, value = node.decision_condition
-        if sample[feature] == value:
-            return self._traverse_tree(sample, node.true_branch)
-        else:
-            return self._traverse_tree(sample, node.false_branch)
-
-        ranked_outcomes = sorted(ranked_outcomes, key=lambda x: x[1], reverse=True)
-        return ranked_outcomes
+partner1.pension()
+partner2.pension()
+partner1.comp_pension()
+partner2.comp_pension()
+partner1.diff_pension(partner2)
+partner1.care()
+partner2.care()
+partner1.tax_class_rec(partner2)
+partner1.mental_load_burden(partner2)
+partner2.mental_load_burden(partner1)
+partner1.income_stats()
+partner2.income_stats()
